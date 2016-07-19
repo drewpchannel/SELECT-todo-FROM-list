b34a381c4b9098f54e6db774070f8329efadb6dd
dpcc <drewpcchannel@gmail.com>
2016-07-18 19:01:25 -1000

    8

--- todo_app.sql
+++ todo_app.sql
@@ -15,9 +15,16 @@ CREATE DATABASE todo_app;
 
 --67
 CREATE TABLE tasks(
-  id serial PRIMARY KEY
+  id serial PRIMARY KEY,
+  title varchar(40),
+  description text,
+  created_at timestamp DEFAULT now(),
+  updated_at timestamp null,
+  completed BOOLEAN DEFAULT false
 );
 --8
 ALTER TABLE IF EXISTS tasks DROP COLUMN IF EXISTS id;
-
+ALTER TABLE IF EXISTS tasks ADD COLUMN completed_at timestamp DEFAULT null;
+ALTER TABLE IF EXISTS tasks ALTER COLUMN updated_at SET NOT NULL;
+ALTER TABLE IF EXISTS tasks ALTER COLUMN updated_at SET DEFAULT now();
 --9
