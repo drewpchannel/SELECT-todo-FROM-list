d19d6fcf3469d4f7d23889351a92c16f8aef818b
dpcc <drewpcchannel@gmail.com>
2016-07-18 19:22:22 -1000

    8

--- todo_app.sql
+++ todo_app.sql
@@ -27,4 +27,7 @@ ALTER TABLE IF EXISTS tasks DROP COLUMN IF EXISTS id;
 ALTER TABLE IF EXISTS tasks ADD COLUMN completed_at timestamp DEFAULT null;
 ALTER TABLE IF EXISTS tasks ALTER COLUMN updated_at SET NOT NULL;
 ALTER TABLE IF EXISTS tasks ALTER COLUMN updated_at SET DEFAULT now();
---9
+INSERT INTO tasks (title, description, created_at, updated_at, completed_at)
+VALUES ('Study SQL', 'Complete this exercise', now(), now(), NULL);
+INSERT INTO tasks (title, description)
+VALUES ('Study PostgreSQL', 'Read all the documentation');
